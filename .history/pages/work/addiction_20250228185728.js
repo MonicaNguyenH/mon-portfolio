82df730f992bb1a457ab1f9f65ca31a0b25e69dc
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2025-02-28 19:00:44 -0800

    Addicted link updated

---
+++ .history/pages/work/addiction_20250228185728.js
@@ -0,0 +1,38 @@
+import styles from '@/styles/Addicted.module.css';
+import HeaderArea from '@/components/HeadArea';
+import NavBar from '@/components/NavBar';
+import Footer from '@/components/Footer';
+import ProjectHeader from '@/components/ProjectHeader';
+import VideoPlayer from '@/components/VideoPlayer';
+
+
+export default function Addicted() {
+    const projectTools = ["After Effects", "Adobe Illustrator"];
+
+    return (
+        <> 
+            <div className={styles.blendingMode}>
+                <HeaderArea title="Addicted" description="Addicted"/>
+                <NavBar />
+
+                <div className={styles.main}>
+                    <ProjectHeader 
+                        name="Addicted" 
+                        description="MOTION GRAPHIC" 
+                        img="/img/graphic/addict/TN.webp" 
+                        introduction={`Addicted is a motion graphic exploring the cycle of prescription drug addiction through minimalist visuals and smooth transitions. It follows a character's journey from a bike accident to opioid dependence, using metaphorical storytelling and fluid animation to simplify complex medical concepts into an engaging visual narrative.`}
+                        tools={projectTools}
+                        date="Oct 2024"
+                    />
+
+                    <div className={styles.video}>
+                        <VideoPlayer />
+                    </div>
+                </div>
+
+                <Footer />
+            </div>
+ 
+        </>
+    )
+}
--- pages/work/addicted.js
+++ pages/work/addicted.js
@@ -1,4 +1,4 @@
-import styles from '@/styles/Addiction.module.css';
+import styles from '@/styles/Addicted.module.css';
 import HeaderArea from '@/components/HeadArea';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
@@ -6,23 +6,23 @@ import ProjectHeader from '@/components/ProjectHeader';
 import VideoPlayer from '@/components/VideoPlayer';
 
 
-export default function Addiction() {
+export default function Addicted() {
     const projectTools = ["After Effects", "Adobe Illustrator"];
 
     return (
         <> 
             <div className={styles.blendingMode}>
-                <HeaderArea title="Addiction" description="Addiction"/>
+                <HeaderArea title="Addicted" description="Addicted"/>
                 <NavBar />
 
                 <div className={styles.main}>
                     <ProjectHeader 
-                        name="Addiction" 
+                        name="Addicted" 
                         description="MOTION GRAPHIC" 
                         img="/img/graphic/addict/TN.webp" 
                         introduction={`Addicted is a motion graphic exploring the cycle of prescription drug addiction through minimalist visuals and smooth transitions. It follows a character's journey from a bike accident to opioid dependence, using metaphorical storytelling and fluid animation to simplify complex medical concepts into an engaging visual narrative.`}
                         tools={projectTools}
-                        date="Oct 2024"
+                        date="Apr 2024"
                     />
 
                     <div className={styles.video}>
